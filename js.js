5dbd94f2a72b779de85cc6532e4c098ce43083bd
AdamGerdvall <89906415+AdamGerdvall@users.noreply.github.com>
2021-10-08 11:53:25 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,10 @@
+var step1 = (()=> {
+    document.getElementById('Obj1').innerHTML = 'Sike!';
+    document.getElementById('Obj1').style.fontSize='60px';
+    document.getElementById('Obj2').style.display = 'block';
+    document.getElementById('Obj3').style.display = 'none';
+    document.getElementById('Obj1').style.color = '#FF0004' 
+})
+function ShowPrism(){
+    document.getElementById('Prism').style.display = 'block'
+}
